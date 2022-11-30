3eb1ac00f52087ceea0da4c97bbb2ad8a4478f35
chenqiongyao <chenqiongyao@agora.io>
2022-11-30 21:22:29 +0800

    添加yuv 采集

--- app/src/main/java/com/coocaa/mediacodec/MainActivity4.java
+++ app/src/main/java/com/coocaa/mediacodec/MainActivity4.java
@@ -10,6 +10,7 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.media.Image;
 import android.media.ImageReader;
 import android.media.MediaCodec;
@@ -19,6 +20,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
+import android.util.Size;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.Window;
@@ -26,7 +28,6 @@ import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.ImageProxy;
 
 import com.coocaa.mediacodec.util.Permission;
 
@@ -35,13 +36,13 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.concurrent.LinkedBlockingQueue;
 
-
+@SuppressWarnings("deprecation")
 public class MainActivity4 extends AppCompatActivity {
     private static final String TAG = "yao";
     private static final int MIN_BITRATE_THRESHOLD = 4 * 1024 * 1024;  //bit per second，每秒比特率
 
     private static final int MAX_VIDEO_FPS = 30;   //frames/sec
-    private static final int I_FRAME_INTERVAL = 10;  //关键帧频率，10秒一个关键帧
+    private static final int I_FRAME_INTERVAL = 1;  //关键帧频率，10秒一个关键帧
 
     private TextureView mPreviewView;
 
@@ -55,10 +56,8 @@ public class MainActivity4 extends AppCompatActivity {
 
     // camera
     private int mCameraId = CameraCharacteristics.LENS_FACING_FRONT; // 要打开的摄像头ID
-//    private final Size mPreviewSize = new Size(1280, 720); // 固定1280*720演示
 
-    private int mWidth;
-    private int mHeight;
+    private int mWidth, mHeight;
     private Context mContext;
 
     private CameraCaptureSession mCaptureSession;
@@ -127,9 +126,7 @@ public class MainActivity4 extends AppCompatActivity {
             Log.i(TAG, "onSurfaceTextureAvailable:  width = " + width + ", height = " + height);
             // mPreviewSize必须先初始化完成
             surface.setDefaultBufferSize(width, height);
-            mWidth = width;
-            mHeight = height;
-            openCamera();
+            openCamera(width, height);
             mPreviewSurface = new Surface(surface);
             startMediaCodecEncoder();
             initPreviewRequest();
@@ -161,13 +158,6 @@ public class MainActivity4 extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         mContext = this;
         setContentView(R.layout.activity_main4);
-//        DisplayMetrics dm = new DisplayMetrics();
-//        Display mDisplay = ((WindowManager) getSystemService(Context.WINDOW_SERVICE))
-//                .getDefaultDisplay();
-//        mDisplay.getMetrics(dm);
-//        mWidth = dm.widthPixels / 2;
-//        mHeight = dm.heightPixels;
-
         mSendQueue = new LinkedBlockingQueue<>();
 
         mPreviewView = findViewById(R.id.cameraSurface);
@@ -221,7 +211,7 @@ public class MainActivity4 extends AppCompatActivity {
                 mWidth, mHeight);
         mediaFormat.setInteger(MediaFormat.KEY_BIT_RATE, MIN_BITRATE_THRESHOLD);//500kbps
         mediaFormat.setInteger(MediaFormat.KEY_FRAME_RATE, MAX_VIDEO_FPS);
-        mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420Flexible); //COLOR_FormatSurface
+        mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatYUV420SemiPlanar); //COLOR_FormatSurface
         mediaFormat.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, I_FRAME_INTERVAL);
         mEncoder.configure(mediaFormat, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
         mEncoder.start();
@@ -265,13 +255,57 @@ public class MainActivity4 extends AppCompatActivity {
         }
     };
 
+    private static Size findClosestSizeInArray(Size[] sizes, int width, int height) {
+        if (sizes == null) return null;
+        Size closestSize = null;
+        int minDiff = Integer.MAX_VALUE;
+        for (Size size : sizes) {
+            final int diff = ((width > 0) ? Math.abs(size.getWidth() - width) : 0)
+                    + ((height > 0) ? Math.abs(size.getHeight() - height) : 0);
+            if (diff < minDiff && size.getWidth() % 32 == 0) {
+                minDiff = diff;
+                closestSize = size;
+            }
+        }
+        if (minDiff == Integer.MAX_VALUE) {
+            Log.e(TAG, "Couldn't find resolution close to (" + width + "x" + height + ")");
+            return null;
+        }
+        return closestSize;
+    }
+
 
     @SuppressLint("MissingPermission")
-    public void openCamera() {
+    public void openCamera(int width, int height) {
         Log.v(TAG, "openCamera");
         startBackgroundThread(); // 对应 releaseCamera() 方法中的 stopBackgroundThread()
         try {
             CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+            try {
+                final CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(String.valueOf(mCameraId));
+                final StreamConfigurationMap streamMap =
+                        cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+                // Find closest supported size.
+                final Size[] supportedSizes = streamMap.getOutputSizes(ImageFormat.YUV_420_888);
+                final Size closestSupportedSize = findClosestSizeInArray(supportedSizes, width, height);
+                if (closestSupportedSize == null) {
+                    Log.e(TAG, "No supported resolutions.");
+                    return;
+                }
+                Log.d(TAG, "allocate: matched (" + closestSupportedSize.getWidth() + " x "
+                        + closestSupportedSize.getHeight() + ")");
+                mWidth = closestSupportedSize.getWidth();
+                mHeight = closestSupportedSize.getHeight();
+
+                Log.d(TAG, "PreviewWidth= " + mWidth + "  mHeight=" + mHeight);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            cameraManager.getCameraCharacteristics(String.valueOf(mCameraId));
             mImageReader = ImageReader.newInstance(mWidth, mHeight, ImageFormat.YUV_420_888, 2);
             mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, null);
             // 打开摄像头
@@ -288,8 +322,11 @@ public class MainActivity4 extends AppCompatActivity {
     public void encoderH264(Image image) {
         //将NV21编码成NV12
         byte[] nv21 = YUV_420_888toNV21(image);
-        //byte[] nv12 = NV21ToNV12(nv21, mWidth, mHeight);
-        byte[] nv12 = nv21;
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+        Log.d(TAG, "image width=" + width + "  height=" + height);
+        byte[] nv12 = NV21ToNV12(nv21, width, height);
 
         int inputBufferIndex = mEncoder.dequeueInputBuffer(timeoutUs);
         if (inputBufferIndex < 0) {
@@ -328,99 +365,6 @@ public class MainActivity4 extends AppCompatActivity {
         mEncoder.releaseOutputBuffer(outputBufferIndex, false);
     }
 
-    private byte[] YUV_420_888toNV21_new(Image image) {
-//        int width = image.getWidth();
-//        int height = image.getHeight();
-
-        int width = mWidth;
-        int height = mHeight;
-        int ySize = width * height;
-        int uvSize = width * height / 4;
-        byte[] mBuffer = new byte[ySize + uvSize * 2];
-
-
-        ByteBuffer yBuffer = image.getPlanes()[0].getBuffer(); // Y
-        ByteBuffer uBuffer = image.getPlanes()[1].getBuffer(); // U
-        ByteBuffer vBuffer = image.getPlanes()[2].getBuffer(); // V
-
-        int rowStride = image.getPlanes()[0].getRowStride();
-        assert (image.getPlanes()[0].getPixelStride() == 1);
-
-        int pos = 0;
-
-        if (rowStride == width) { // likely
-            yBuffer.get(mBuffer, 0, ySize);
-            pos += ySize;
-        } else {
-            int yBufferPos = width - rowStride; // not an actual position
-            for (; pos < ySize; pos += width) {
-                yBufferPos += rowStride - width;
-                yBuffer.position(yBufferPos);
-                yBuffer.get(mBuffer, pos, width);
-            }
-        }
-
-        rowStride = image.getPlanes()[2].getRowStride();
-        int pixelStride = image.getPlanes()[2].getPixelStride();
-
-        assert (rowStride == image.getPlanes()[1].getRowStride());
-        assert (pixelStride == image.getPlanes()[1].getPixelStride());
-
-        if (pixelStride == 2 && rowStride == width && uBuffer.get(0) == vBuffer.get(1)) {
-            // maybe V an U planes overlap as per NV21, which means vBuffer[1] is alias of uBuffer[0]
-            byte savePixel = vBuffer.get(1);
-            vBuffer.put(1, (byte) 0);
-            if (uBuffer.get(0) == 0) {
-                vBuffer.put(1, (byte) 255);
-                if (uBuffer.get(0) == 255) {
-                    vBuffer.put(1, savePixel);
-                    vBuffer.get(mBuffer, ySize, uvSize);
-
-                    return mBuffer; // shortcut
-                }
-            }
-
-            // unfortunately, the check failed. We must save U and V pixel by pixel
-            vBuffer.put(1, savePixel);
-        }
-
-        // other optimizations could check if (pixelStride == 1) or (pixelStride == 2),
-        // but performance gain would be less significant
-
-        for (int row = 0; row < height / 2; row++) {
-            for (int col = 0; col < width / 2; col++) {
-                int vuPos = col * pixelStride + row * rowStride;
-                mBuffer[pos++] = vBuffer.get(vuPos);
-                mBuffer[pos++] = uBuffer.get(vuPos);
-            }
-        }
-
-        return mBuffer;
-    }
-
-
-    /**
-     * 将NV21格式数据转换为NV12格式数据
-     * NV12与NV21类似，U 和 V 交错排列,不同在于UV顺序。
-     * NV12: YYYYYYYY UVUV =>YUV420SP
-     * NV21: YYYYYYYY VUVU =>YUV420SP
-     */
-    private void NV21ToNV12(byte[] nv21, byte[] nv12, int width, int height) {
-
-        if (nv21 == null || nv12 == null) return;
-        int frameSize = width * height;
-        int i = 0, j = 0;
-        System.arraycopy(nv21, 0, nv12, 0, frameSize);
-        for (i = 0; i < frameSize; i++) {
-            nv12[i] = nv21[i];
-        }
-        for (j = 0; j < frameSize / 2; j += 2) {
-            nv12[frameSize + j - 1] = nv21[j + frameSize];
-        }
-        for (j = 0; j < frameSize / 2; j += 2) {
-            nv12[frameSize + j] = nv21[j + frameSize - 1];
-        }
-    }
 
     /**
      * 因为从MediaCodec不支持NV21的数据编码，所以需要先讲NV21的数据转码为NV12
@@ -506,40 +450,6 @@ public class MainActivity4 extends AppCompatActivity {
         return bytes;
     }
 
-
-    /**
-     * YUV_420_888转NV21
-     *
-     * @param image CameraX ImageProxy
-     * @return byte array
-     */
-    public static byte[] yuv420ToNv21(ImageProxy image) {
-        ImageProxy.PlaneProxy[] planes = image.getPlanes();
-        ByteBuffer yBuffer = planes[0].getBuffer();
-        ByteBuffer uBuffer = planes[1].getBuffer();
-        ByteBuffer vBuffer = planes[2].getBuffer();
-        int ySize = yBuffer.remaining();
-        int uSize = uBuffer.remaining();
-        int vSize = vBuffer.remaining();
-        int size = image.getWidth() * image.getHeight();
-        byte[] nv21 = new byte[size * 3 / 2];
-        yBuffer.get(nv21, 0, ySize);
-        vBuffer.get(nv21, ySize, vSize);
-
-        byte[] u = new byte[uSize];
-        uBuffer.get(u);
-
-        //每隔开一位替换V，达到VU交替
-        int pos = ySize + 1;
-        for (int i = 0; i < uSize; i++) {
-            if (i % 2 == 0) {
-                nv21[pos] = u[i];
-                pos += 2;
-            }
-        }
-        return nv21;
-    }
-
     public void releaseCamera() {
         Log.v(TAG, "releaseCamera");
         if (mImageReader != null) {
@@ -596,13 +506,6 @@ public class MainActivity4 extends AppCompatActivity {
         }
     }
 
-
-    public void switchCamera() {
-        mCameraId ^= 1;
-        Log.d(TAG, "switchCamera: mCameraId: " + mCameraId);
-        releaseCamera();
-        openCamera();
-    }
 
     private void startBackgroundThread() {
         if (mBackgroundThread == null || mBackgroundHandler == null) {
